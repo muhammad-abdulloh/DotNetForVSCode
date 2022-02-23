8b65a57684980ad9a8421d62c9b02d7fc0de9e62
Clever Coder <92303840+clevercoderr@users.noreply.github.com>
2022-02-23 18:18:36 +0500

    sudralma

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DotNetForVSCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 0; i<5; i++){
+                string some = Console.ReadLine();
+                Console.WriteLine(some);
+            }
+        }
+    }
+}
